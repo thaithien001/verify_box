0e01e00c59fc09bb66a69633aace43a163cd70e7
thaithien001 <thaithien001@gmail.com>
2021-02-08 16:39:08 +0700

    update

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from setuptools import setup
 setup(name='verify_box',
       version='0.0.1',
       description='Calculate accuracy between boxes in a image',
-      url='',
+      url='https://github.com/thaithien001/verify_box',
       author='Thien Ng',
       author_email='thien.nguyenthai.ncc@gmail.com',
       license='MIT',
-      packages=['verify_box'],
+      packages=['accuracy'],
+      install_requires=['numpy'],
+      python_requires='~=3.3',
       zip_safe=False)
